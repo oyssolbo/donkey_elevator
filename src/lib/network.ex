06d4bff16868f866988afc8f7e14cf3f1ffc27d6
S.Anntid <sanntid@sanntid>
2021-02-24 10:22:54 +0100

    Added function to detect nodes on network
    Signed-off-by: S.Anntid <sanntid@sanntid>

--- src/lib/network.ex
+++ src/lib/network.ex
@@ -1,210 +1,79 @@
-defmodule Server do
+defmodule Network do
   @moduledoc """
-  The Server-module is the server that the master runs for controlling
-  the elevators in the project.
+  Module giving basic functions for using networking
 
-  The module is supervised via a supervisor to make sure that the system
-  experiences good concurrency.
+  Entire module inspired by Jostein Løwer
+
+  Credit to: Jostein Løwer, NTNU (2019)
+  Link: https://github.com/jostlowe/kokeplata/blob/master/lib/networkstuff.ex
   """
 
-  # Require Logger to write a log instead of writing only to screen
   require Logger
 
-  # Read parameters from a list of IP and PORT-addresses ( TODO )
-  @default_server_port 20012
-  @default_local_host {255, 255, 255, 255}
-  @default_timeout 500
+  @broadcast_address {255, 255, 255, 255}
+  @init_port 6789
+  @num_tries 5
 
 
   @doc """
-  Launches the server with an integer value for the port
+  @brief        Function that hopefully returns the IP-address of the system
 
-  launch_server/0 defaults launch_server/1 to @default_server_port
+  @p port       Port we should try to access. Default param set to @init_port
 
-  launch_server/1 takes in a port with value 'port' to launch the server
-  at
+  @retval       RETURNS:                        IF:
+                  ip                              If the IP-address was found
+                  {:error, :could_not_get_ip}     If the IP-address could not be
+                                                    resolved
   """
-  def launch_server() do
-    launch_server( @default_server_port )
-  end
+  def get_ip(port \\ @init_port) do
+    case UDP.open_connection(port, [active: false, broadcast: true]) do
+      {:ok, socket} ->
+        UDP.send_data(socket, @broadcast_address, port, "Test")
 
+        case UDP.receive_data(socket) do
+          {:recv, {ip, _port, _data}} ->
+            {:recv, ip}
+          {:error, _} ->
+            {:error, :could_not_get_ip}
+        end
 
-  def launch_server( port ) when port |> is_integer do
-    # Gets a kernel-error when using binary: :true as an option
-    case :gen_udp.open( @default_server_port, [ :binary, active: :false, reuseaddr: :true ] ) do
-      { :ok, socket } ->
-        IO.puts("Listening on port")
-        listen( socket )
+        UDP.close_socket(socket)
 
-      { :error, reason } ->
-        IO.puts( "Could not open the port #{port} due to #{reason}" )
-        Logger.error( "Could not open the port #{port} due to #{reason}" )
-
+        {:nil, _} ->
+          if port - @init_port < @num_tries do
+            get_my_ip(port + 1)
+          else
+            Logger.error("Could not find ip-address")
+            {:error, :could_not_get_ip}
+          end
     end
   end
 
 
   @doc """
-  Shut down a socket if an error has occured
+  @brief        Formats an IP-address to a bytestring
+
+  @p ip         IP-address to convert to a bytestring
   """
-  defp shut_down_socket( socket ) do
-    :gen_udp.close( socket )
-  end
-
-
-  @doc """
-  Listen for new messages. Spawn a new process to handle the data
-  before
-  """
-  defp listen( socket ) do
-    case :gen_udp.recv( socket, 0 ) do
-      { :ok, recv_data } ->
-        IO.puts("Data received")
-        spawn( fn -> serve_message( recv_data, socket ) end )
-
-      { :error, reason } ->
-        Logger.error( "Error occured when listening on socket #{socket} due to #{reason}" )
-
-    end
-
-    listen( socket )
-  end
-
-
-  @doc """
-  Serve any incomming message. This must be updated according to the
-  requirements
-  """
-  defp serve_message( { :udp, _socket, ip, port, data }, socket ) do
-    # Send an acknowledgement that the data is received
-    IO.puts("Serving message")
-    dest = { ip, port }
-    result = factorial( String.to_integer( data ) )
-    send_data( socket, dest, Integer.to_string( result ) )
-  end
-
-
-  defp serve_message( _, socket ) do
-    Logger.error( "Unidentified message received" )
-  end
-
-
-  @doc """
-  Calculating the factorial of the given number
-
-  Just a random function to process the data given to the server. Must be
-  replaced by a real function later
-  """
-  defp factorial( N ) when N <= 0 do
-    1
-  end
-
-  defp factorial( N ) when N > 0 and N |> is_integer do
-    N * factorial( N - 1 )
-  end
-
-
-  @doc """
-  Function to send a package 'data' to a destination 'dest'.
-  It is assumed that 'dest' is a tuple containing { ip-address, port }
-  """
-  defp send_data( socket, dest, data ) when socket |> is_tuple do
-    :gen_udp.send( socket, dest, data )
-  end
-end
-
-
-defmodule Client do
-  @moduledoc """
-  The Client-module implements the functions for the clients connecting to
-  the server. Mainly used for the elevators connected to the masters
-  """
-
-  # Require Logger to write a log instead of writing only to screen
-  require Logger
-
-  # Read parameters from a list of IP and PORT-addresses ( TODO )
-  @default_server_ip {127,0,0,1}
-  @default_server_port 20013
-  @default_timeout 1000
-
-  @client_port @default_server_port + 1
-
-  @doc """
-  Function to initiate the port and connect to the server
-  """
-  def initiate_client() do
-    case :gen_udp.open( @client_port, [:binary, active: :true, reuseaddr: :true] ) do
-      { :ok, socket } ->
-        IO.puts("Spawning")
-        spawn( fn -> send_data( socket, 5 ) end )
-        spawn( fn -> loop( socket ) end )
-
-
-      { :error, reason } ->
-        IO.puts( "Could not open the port due to #{reason}" )
-        Logger.error( "Could not open the port due to #{reason}" )
-    end
-  end
-
-
-  @doc """
-  Loop to just check for connection to the server
-  """
-  defp loop( socket ) do
-    case :gen_udp.recv( socket, 0 ) do
-      {:ok, recv_data} ->
-        IO.puts("Received data")
-
-        { _, _, packet: packet } = recv_data
-
-        IO.puts("Received the packet #{packet}")
-
-      {:error, reason} ->
-        loop(socket)
-      end
-  end
-
-  @doc """
-  Function to send an int to the server
-  """
-  defp send_data( socket, data ) when data |> is_integer and data >= 0 do
-    ip = get_my_ip() |> ip_to_string() # Since on the same machine
-    dest = { ip, @default_server_port }
-    data_string = Integer.to_string( data )
-    IO.puts("Trying to send data")
-
-    case :gen_tcp.send( socket, dest, data_string ) do
-      :ok ->
-        send_data( socket, data - 1 )
-        IO.puts("Sending data")
-        :ok
-      { :error, reason } ->
-        IO.puts( "Error occured due to #{reason} under transmit" )
-    end
-  end
-
-  defp get_my_ip(counter \\ 0) when counter < 11 do
-    Process.sleep(100)
-
-    if counter == 10 do
-      IO.puts("Couldn't find my IP")
-    end
-
-    {:ok, socket} = :gen_udp.open(6199, active: false, broadcast: true)
-    :ok = :gen_udp.send(socket, {255, 255, 255, 255}, 6199, "Test packet")
-
-    ip =
-      case :gen_udp.recv(socket, 100, 1000) do
-        {:ok, {ip, _port, _data}} -> ip
-        {:error, _} -> get_my_ip(counter + 1)
-      end
-
-    :gen_udp.close(socket)
-    ip
-  end
-
-  defp ip_to_string(ip) do
+  def ip_to_string(ip) do
     :inet.ntoa(ip) |> to_string()
+  end
+
+
+
+  @doc """
+  @brief        Detects all nodes on the network
+
+  @retval       RETURNS:                      IF:
+                  node_list                     If nodes discovered
+                  {:error, :node_not_running}   If nodes not running
+  """
+  def detect_nodes() do
+    case [Node.self() | Node.list()] do
+      [:'nonode@nohost'] ->
+        {:error, :node_not_running}
+      nodes ->
+        nodes
+    end
   end
 end

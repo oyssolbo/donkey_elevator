bee1e4f494782932192f363f39626af2896776b2
Øystein Solbø <oyssolbo@stud.ntnu.no>
2021-03-03 07:06:30 +0100

    Rewrote panel.ex from cambelCase to snake_case
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- src/lib/panel.ex
+++ src/lib/panel.ex
@@ -7,41 +7,41 @@ defmodule Panel do
     @state_map  %{:on => true, :off => false}
 
     def init(mid, eid) do
-        checkerID = spawn(fn -> orderChecker(Matriks.falseOrderMatrix) end)
-        senderID = spawn(fn -> orderSender(mid, eid, checkerID, 0, Matriks.falseOrderMatrix) end)
+        checkerID = spawn(fn -> order_checker(Matriks.false_order_matrix) end)
+        senderID = spawn(fn -> order_sender(mid, eid, checkerID, 0, Matriks.false_order_matrix) end)
         {senderID, checkerID}
     end
 
 
-    defp orderChecker(oldOrderMatrix) do
+    defp order_checker(old_order_matrix) do
 
         # Update order matrix by reading all HW order buttons
-        [newUp, newDown, newCab] = [upChecker, downChecker, cabChecker]
-        newOrderMatrix = Matriks.from_list([newUp, newDown, newCab])
-        updatedMatrix = Matriks.orderMatrixOR(oldOrderMatrix, newOrderMatrix)
+        [new_up, new_down, new_cab] = [up_checker, down_checker, cab_checker]
+        new_order_matrix = Matriks.from_list([new_up, new_down, new_cab])
+        updated_matrix = Matriks.orderMatrixOR(old_order_matrix, new_order_matrix)
 
         # Check for request from sender. If there is, send order matrix and recurse with reset matrix
         receive do
             {:gibOrdersPls, senderAddr} ->
-                send(senderAddr, {:orderChecker, updatedMatrix})
-                orderChecker(Matriks.falseOrderMatrix)
+                send(senderAddr, {:order_checker, updated_matrix})
+                order_checker(Matriks.false_order_matrix)
             after
                 0 -> :ok
         end
 
         # If no send request, requrse with current matrix (output buffer)
-        orderChecker(updatedMatrix)
+        order_checker(updated_matrix)
     end
 
-    defp orderSender(mid, eid, checkerAddr, sendID, outGoingMatrix) do
+    defp order_sender(mid, eid, checker_addr, sendID, outgoing_matrix) do
 
         # If the order matrix isnt empty ...
-        if outGoingMatrix != Matriks.falseOrderMatrix do
+        if outgoing_matrix != Matriks.false_order_matrix do
 
             # ... send the respective  orders to master and elevator
             # Should have an enum or variable indicating idx for master and idx for elevator
-            send(mid, {self(), :newOrders, sendID, [Matriks.to_list(outGoingMatrix[0]), Matriks.to_list(outGoingMatrix[1])]})
-            send(eid, {self(), :newOrders, sendID, Matriks.to_list(outGoingMatrix[2])})
+            send(mid, {self(), :newOrders, sendID, [Matriks.to_list(outgoing_matrix[0]), Matriks.to_list(outgoing_matrix[1])]})
+            send(eid, {self(), :newOrders, sendID, Matriks.to_list(outgoing_matrix[2])})
 
             # ... and wait for an ack
             receive do
@@ -51,39 +51,39 @@ defmodule Panel do
                     Shouldn't really be necessary! See what I wrote on miro
                     """
                     send(from, {:ack, sentID})
-                    send(checkerAddr, {:gibOrdersPls, self()})
+                    send(checker_addr, {:gibOrdersPls, self()})
                     receive do
                         # When latest order matrix is received, recurse with new orders and iterated sendID
-                        {:orderChecker, updatedMatrix} ->
-                            orderSender(mid, eid, checkerAddr, sendID+1, updatedMatrix)
+                        {:order_checker, updated_matrix} ->
+                            order_sender(mid, eid, checker_addr, sendID+1, updated_matrix)
                     end
                 # If no ack is received after 1.5 sec: Recurse and repeat
                 after
                     # Should have the timer as a standard-value
-                    1500 -> orderSender(mid, eid, checkerAddr, sendID, outGoingMatrix)
+                    1500 -> order_sender(mid, eid, checker_addr, sendID, outgoing_matrix)
             end
 
         else
             # If order matrix is empty, send request to checker for latest orders. Recurse with those (but same sender ID)
-            send(checkerAddr, {:gibOrdersPls, self()})
+            send(checker_addr, {:gibOrdersPls, self()})
                     receive do
-                        {:orderChecker, updatedMatrix} ->
-                            orderSender(mid, eid, checkerAddr, sendID, updatedMatrix)
+                        {:order_checker, updated_matrix} ->
+                            order_sender(mid, eid, checker_addr, sendID, updated_matrix)
                     end
         end
 
     end
 
 
-    defp upChecker do
+    defp up_checker do
         [floor1, floor2, floor3, floor4] = [Enum.random([false,true]), Enum.random([false,true]), Enum.random([false,true]), Enum.random([false,true])]
     end
 
-    defp downChecker do
+    defp down_checker do
         [floor1, floor2, floor3, floor4] = [Enum.random([false,true]), Enum.random([false,true]), Enum.random([false,true]), Enum.random([false,true])]
     end
 
-    defp cabChecker do
+    defp cab_checker do
         [floor1, floor2, floor3, floor4] = [Enum.random([false,true]), Enum.random([false,true]), Enum.random([false,true]), Enum.random([false,true])]
     end
 

5f39b97d93385c3814db674b42ef7f6684942cf1
Øystein Solbø <oyssolbo@stud.ntnu.no>
2021-03-30 19:22:34 +0200

    Start on function for testing master
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

---
+++ elevator_project/lib/test_master.ex
@@ -0,0 +1,21 @@
+defmodule MasterTest do
+  @moduledoc """
+  Module for testing master
+
+  Desired tests:
+    - Calculating optimal elevator to serve an order
+    - Calculating cost for an elevator/order
+    - Getting undelegated orders
+    - Getting orders that are previously set delegated to an elevator if the elevator 'times out'
+    - Transitions from backup to acktive
+
+
+    Requires network:
+    - Testing if two masters are active simultaneously
+    - Communication between an elevator and a master (send status etc.)
+  """
+
+
+
+
+end

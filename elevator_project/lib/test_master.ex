fe065420c9fd65935e2910c93231ab42d903d835
Øystein Solbø <oyssolbo@stud.ntnu.no>
2021-04-01 11:58:56 +0200

    Some changes to test-files
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- elevator_project/lib/test_master.ex
+++ elevator_project/lib/test_master.ex
@@ -15,6 +15,18 @@ defmodule MasterTest do
     - Communication between an elevator and a master (send status etc.)
   """
 
+  def test_find_optimal_elevator()
+  do
+    order_opts = [order_id: Timer.get_utc_time(), order_type: :cab, order_floor: 2]
+    order = struct(Order, order_opts)
+
+    elevator_id = Timer.get_utc_time()
+
+    elevator_data = %{dir: :down, last_floor: 3, elevator_id: elevator_id}
+    elevator_client = struct(Client, [client_data: elevator_data])
+
+    id = Master.find_optimal_elevator(order, elevator_client, struct(Client))
+  end
 
 
 

1b087a77332b3ca57ae9ed1472f6864b192552f9
Øystein Solbø <oyssolbo@stud.ntnu.no>
2021-04-02 13:45:00 +0200

    Added supervisor for entire project
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

---
+++ elevator_project/lib/application.ex
@@ -0,0 +1,18 @@
+defmodule ElevatorProject.Application do
+  @moduledoc """
+  Entry-point of the elevator-projects. Starts the modules' supervisors,
+  which then again invokes the modules
+  """
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      {ElevatorProject.Supervisor, []}
+    ]
+
+    opts = [strategy: :one_for_one, name: ElevatorProject.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+end

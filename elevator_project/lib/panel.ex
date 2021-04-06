4505cfff2042e1571a60008a2f8f406424467d98
Sindre Øversveen <sindrbov@stud.ntnu.no>
2021-04-06 16:44:50 +0200

    Added the actual changes to panel I forgot to save earlier

--- elevator_project/lib/panel.ex
+++ elevator_project/lib/panel.ex
@@ -46,9 +46,6 @@ defmodule Panel do
         Process.register(dummy, :master)
         #---------------#
 
-        checker_sprvsr_init(floor_table)
-        sender_sprvsr_init(floor_table)
-
         {checker_ID, sender_ID}
     end
 
@@ -74,156 +71,10 @@ defmodule Panel do
         sender_ID        
     end
 
-    defp checker_sprvsr_init(floor_table) do
-        fraudster = Process.whereis(:checker_sprvsr)
-        if fraudster != nil do
-            Process.exit(fraudster, :kill)
-        end
 
-        its_id = spawn(fn -> checker_sprvsr(floor_table) end)
-        Process.register(its_id, :checker_sprvsr)
-        its_id
-    end
-
-    defp sender_sprvsr_init(floor_table) do
-        fraudster = Process.whereis(:sender_sprvsr)
-        if fraudster != nil do
-            Process.exit(fraudster, :kill)
-        end
-
-        its_id = spawn(fn -> sender_sprvsr(floor_table) end)
-        Process.register(its_id, :sender_sprvsr)
-        its_id
-    end
-
-    # SUPERVISORS
-    def checker_sprvsr(floor_table, missed_child_pings \\ 1, missed_buddy_pings \\ 0) do
-        timeout = 1500
-
-        # Send a ping to the other supervisor to let it know you exist
-        Network.send_data_inside_node(:checker_sprvsr, :sender_sprvsr, :ping)
-
-        # Check how many pings have been missed. If above 5, run init on the relevant process
-        cond do
-            missed_child_pings > 4 ->
-                IO.inspect("Timed out on #{inspect missed_child_pings} pings from child. Initialized new checker.", label: "Checker Supervisor")
-                init_checker(floor_table)
-                checker_sprvsr(floor_table, 0, missed_buddy_pings)
-            missed_buddy_pings > 4 ->
-                IO.inspect("Timed out on #{inspect missed_buddy_pings} pings from buddy. Initialized new supervisor.", label: "Checker Supervisor")
-                sender_sprvsr_init(floor_table)  
-                checker_sprvsr(floor_table, missed_child_pings, 0)
-            missed_child_pings < 5 and missed_buddy_pings < 5 ->
-                :ok              
-        end
-
-        # Listen for ping from child process. If none is heard within Timeout, add +1 to missed_child_pings
-        receive do
-            {:order_checker, {_message_id, :ping}} ->
-                send(self(), {:mcp, 0})
-            after
-                timeout -> 
-                    mcp = missed_child_pings + 1
-                    send(self(), {:mcp, mcp})
-                    IO.inspect("Timed out on ping from child (Missed #{inspect mcp} total).", label: "Checker Supervisor")
-        end        
-
-        # Listen for ping from other supervisor. If none heard within Timeout, add +1 to missed_buddy_pings
-        receive do
-            {:sender_sprvsr, {_message_id, :ping}} ->
-                send(self(), {:mbp, 0})
-            after
-                2*timeout -> 
-                    mbp = missed_buddy_pings + 1
-                    send(self(), {:mbp, mbp})
-                    IO.inspect("Timed out on ping from buddy (Missed #{inspect mbp} total).", label: "Checker Supervisor")
-
-        end  
-
-        # Recurse with updated missed pings
-        receive do
-            {:mcp, mcp} ->
-                receive do
-                    {:mbp, mbp} ->
-                        checker_sprvsr(floor_table, mcp, mbp)
-                    after
-                        0 -> :ok
-                end
-            after
-                0 -> :ok
-        end
-        
-        IO.inspect("Internal error: No messages from self received. Reset missed pings", label: "Checker Supervisor")
-        checker_sprvsr(floor_table)        
-    end
-
-    defp sender_sprvsr(floor_table, missed_child_pings \\ 0, missed_buddy_pings \\ 0) do
-
-        timeout = 1500
-        #IO.puts("Senderloop start; mcp = #{inspect missed_child_pings}, mbp = #{inspect missed_buddy_pings}")
-
-        # Send a ping to the other supervisor to let it know you exist
-        Network.send_data_inside_node(:sender_sprvsr, :checker_sprvsr, :ping)
-
-        # Check how many pings have been missed. If above 5, run init on the relevant process
-        cond do
-            missed_child_pings > 4 ->
-                IO.inspect("Timed out on #{inspect missed_child_pings} pings from child. Initialized new sender.", label: "Sender Supervisor")
-                init_sender(floor_table)
-                sender_sprvsr(floor_table, 0, missed_buddy_pings)
-            missed_buddy_pings > 4 ->
-                IO.inspect("Timed out on #{inspect missed_buddy_pings} pings from buddy. Initialized new supervisor.", label: "Sender Supervisor")
-                checker_sprvsr_init(floor_table)  
-                sender_sprvsr(floor_table, missed_child_pings, 0)
-            missed_child_pings < 5 and missed_buddy_pings < 5 ->
-                :ok               
-        end
-
-        # Listen for ping from child process. If none is heard within Timeout, add +1 to missed_child_pings
-        receive do
-            {:panel, {_message_id, :ping}} ->
-                send(self(), {:mcp, 0})
-            after
-                timeout -> 
-                    mcp = missed_child_pings + 1
-                    send(self(), {:mcp, mcp})
-                    IO.inspect("Timed out on ping from child (Missed #{inspect mcp} total).", label: "Sender Supervisor")
-        end        
-
-        # Listen for ping from other supervisor. If none heard within Timeout, add +1 to missed_buddy_pings
-        receive do
-            {:checker_sprvsr, {_message_id, :ping}} ->
-                send(self(), {:mbp, 0})
-            after
-                2*timeout -> 
-                    mbp = missed_buddy_pings + 1
-                    send(self(), {:mbp, mbp})
-                    IO.inspect("Timed out on ping from buddy (Missed #{inspect mbp} total).", label: "Sender Supervisor")
-        end  
-
-        # Recurse with updated missed pings
-        receive do
-            {:mcp, mcp} ->
-                receive do
-                    {:mbp, mbp} ->
-                        #IO.puts("Senderloop end; mcp = #{inspect mcp}, mbp = #{inspect mbp}")
-                        sender_sprvsr(floor_table, mcp, mbp)
-                    after
-                        0 -> :ok
-                end
-            after
-                0 -> :ok
-        end
-        
-        IO.inspect("Internal error: No messages from self received. Reset missed pings", label: "Sender Supervisor")
-        sender_sprvsr(floor_table)
-    end
-
-    
     # MODULE PROCESSES
 
     defp order_checker(old_orders, floor_table) when is_list(old_orders) do
-        Network.send_data_inside_node(:order_checker, :checker_sprvsr, :ping)
 
         checkerSleep = 200
 
@@ -261,7 +112,6 @@ defmodule Panel do
     end
 
     defp order_sender(floor_table, send_ID, outgoing_orders) when is_list(outgoing_orders) do
-        Network.send_data_inside_node(:panel, :sender_sprvsr, :ping)
         ackTimeout = 800
         checkerTimeout = 1000
 
@@ -270,7 +120,6 @@ defmodule Panel do
             # ... send orders to all masters on network, and send cab orders to local elevator
             Network.send_data_to_all_nodes(:panel, :master, {outgoing_orders, send_ID})
             Network.send_data_inside_node(:panel, :master, Order.extract_cab_orders(outgoing_orders))
-            #send({:elevator, node}, {:cab_orders, :panel, self(), extract_cab_orders(orders)})
 
             # ... and wait for an ack
             receive do
@@ -297,7 +146,6 @@ defmodule Panel do
 
         else
             # If order matrix is empty, send request to checker for latest orders. Recurse with those (but same sender ID)
-            #send(checker_addr, {:gibOrdersPls, self()})
             Network.send_data_inside_node(:panel, :order_checker, :gibOrdersPls)
             receive do
                 {:order_checker, {_messageID, updated_orders}} ->
@@ -339,41 +187,16 @@ defmodule Panel do
     end
 
     ### TEST CODE ###
-    def initialize(func, x, name) do
-        if Process.whereis(name) == nil do
-            dafunc = spawn(fn x -> func.(x) end)
-            Process.register(dafunc, name)
-        else
-            IO.inspect("Function already exists", label: "Function initializer")
-        end
-
-    end
-
 
     def annihilate() do
         checkerID = Process.whereis(:order_checker)
         senderID = Process.whereis(:panel)
-        checkersprvsrID = Process.whereis(:checker_sprvsr)
-        sendersprvsrID = Process.whereis(:sender_sprvsr)
+
         if checkerID != nil do
             Process.exit(checkerID, :kill)
         end
         if senderID != nil do
             Process.exit(senderID, :kill)
-        end
-        if checkersprvsrID != nil do
-            Process.exit(checkersprvsrID, :kill)
-        end
-        if sendersprvsrID != nil do
-            Process.exit(sendersprvsrID, :kill)
-        end
-    end
-
-    def dummy_hardware_order_checker(floor, type) do
-        num = :rand.uniform(10)
-        ordr = []
-        if num > 4 do
-            ordr = Order.gib_rnd_order(floor,type)
         end
     end
 

7bbe4ff8b2d44117da586b69bb596b4aa90368f7
Øystein Solbø <oyssolbo@stud.ntnu.no>
2021-04-12 13:55:06 +0200

    Made a supervisor for lights
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

---
+++ elevator_project/lib/supervisors/lights_supervisor.ex
@@ -0,0 +1,38 @@
+defmodule Lights.Supervisor do
+  @moduledoc """
+  Supervisor supervising the Lights.
+  """
+
+  use Supervisor
+
+  @doc """
+  Starts a link from the supervisor to the module
+  """
+  def start_link(init_arg) do
+    Supervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
+  end
+
+
+  @doc """
+  Function for initializing the supervisor and 'children'
+  """
+  @impl true
+  def init(_init_arg)
+  do
+    children = [
+      {Lights, []}
+    ]
+
+    # one_for_one: Only one module will be spawned at crash
+    # max_seconds: Number of seconds we allow max_restarts to occur.
+    #               Defaults to 5
+    # max_restarts: Number of restarts we allow within max_seconds.
+    #               Defaults to 3
+    opts = [
+      strategy: :one_for_one,
+      max_seconds: 2,
+      name: :lights_supervisor
+    ]
+    Supervisor.init(children, opts)
+  end
+end

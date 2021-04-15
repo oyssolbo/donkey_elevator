f1ca15dfaf5fa49098e376209016bb69e3f8bfe3
Your Name <you@example.com>
2021-04-15 09:49:10 +0200

    updated testscript

--- elevator_project/lib/test_scripts/test_network_elevator_master.ex
+++ elevator_project/lib/test_scripts/test_network_elevator_master.ex
@@ -3,8 +3,8 @@ defmodule NetworkTest do
   # Should probably start init as a new process, will fix tomorrow
   def init()
   do
-    Driver.start_link([])
-    #Network.init_node_network()
+    #Driver.start_link([])
+    Network.init_node_network()
     Elevator.start_link([])
     Master.start_link([])
     Panel.start_link([])
